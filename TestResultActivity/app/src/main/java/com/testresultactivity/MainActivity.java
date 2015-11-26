8f3ee86ae69ee0a04d10acb9744bf7f8d5d2b6ba
KaiLuen <stonegray1@yahoo.com.tw>
2015-11-26 22:51:36 +0800

    Modify

--- TestResultActivity/app/src/main/java/com/testresultactivity/MainActivity.java
+++ TestResultActivity/app/src/main/java/com/testresultactivity/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends Activity {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode != 0) {
             AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-            builder = builder.setMessage("User Denied").setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            builder = builder.setMessage("Error").setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                 }
